8e701490891c969d06af2e0f613dca41d02b8cb3
juan <juanrangel1@mail.weber.edu>
2017-09-26 13:03:24 -0600

    commit

---
+++ task3.cpp
@@ -0,0 +1,18 @@
+// File: task3
+// Created by Juan Rangel Juarez on 9/26/17 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+
+// Constants, Structs, Classes 
+
+// Prototypes
+
+// Main Program Program
+int main(void) {
+#$END$
+    return 0;
+}
+// Function Definitions
