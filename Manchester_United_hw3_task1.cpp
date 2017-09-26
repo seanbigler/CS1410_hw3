e8c5938a727a6bbe32ac7e2b260c0219f672d989
w01164264 <seanbigler@mail.weber.edu>
2017-09-26 13:38:23 -0600

    Created the date class

--- Manchester_United_hw3_task1.cpp
+++ Manchester_United_hw3_task1.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "This is a change!" << endl;
-    cout << "This is another change - Sean" << endl;
+// Constants, Structs, Classes
+class date
+{
+private:
+    int month;
+    int day;
+    int year;
+public:
+    void getDate()
+    {
+        int tempchar;
+
+        // Not Working
+        cin >> month >> tempchar >> day >> tempchar >> year;
+    }
+    void showDate()
+    {
+        cout << month << "/" << day << "/" << year << endl;
+    }
+};
+
+// Prototypes
+
+
+// Main Program
+int main()
+{
+    date d1, d2;
+    cout << "Enter a date (format 12/31/99): ";
+    d1.getDate();
+    d1.showDate();
     return 0;
 }
+
+//Function Definitions
