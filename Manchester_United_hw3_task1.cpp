70815c88e75d340a99a4726f2d38d9d2baaea0ce
seanbigler <seanbigler@mail.weber.edu>
2017-09-26 15:11:01 -0600

    Added constructor for default date. Added some comments

--- Manchester_United_hw3_task1.cpp
+++ Manchester_United_hw3_task1.cpp
@@ -9,14 +9,19 @@ private:
     int day;
     int year;
 public:
+    //Constructor
+    date():month(1), day(1), year(2000){}
+
     void getDate()
     {
         char tempchar;
 
+        // Read input from user
         cin >> month >> tempchar >> day >> tempchar >> year;
     }
     void showDate()
     {
+        // Display date separated by /
         cout << month << "/" << day << "/" << year << endl;
     }
 };
@@ -28,15 +33,19 @@ public:
 int main()
 {
     date d1, d2;
+    // Ask for user input for date1
     cout << "Enter a date (format 12/31/1999): ";
     d1.getDate();
+    // Ask for user input for date2
     cout << "Enter another date: ";
     d2.getDate();
 
+    // Display dates
     cout << "date1 = ";
     d1.showDate();
     cout << "date2 = ";
     d2.showDate();
+
     return 0;
 }
 
