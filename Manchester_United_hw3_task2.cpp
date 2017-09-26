13d30af0973ec28ce76012bf6c838347a329ea9f
w01164264 <seanbigler@mail.weber.edu>
2017-09-26 12:54:06 -0600

    Initial commit

---
+++ Manchester_United_hw3_task2.cpp
@@ -0,0 +1,21 @@
+// File: Manchester_United_hw3_task2.cpp
+// Created by Sean Bigler on 9/26/2017 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+// Constants, Structs, Classes
+
+
+// Prototypes
+
+
+// Main Program Program
+int main() {
+
+
+    return 0;
+}
+
+// Function Definitions
